72b6221257a0ff65646a17fb55ae8114f3c8dc49
Marius Kittler <mkittler@suse.de>
2020-03-05 18:06:40 +0100

    WIP: Bring automatic job re-scheduling back
    * Re-schedule jobs only when the worker claims to work on a different
      job to avoid re-introducing the race condition
    * Send the assigned job(s) again to the worker if it claims to work
      on no job at all
    * Simplify the code and tests

--- lib/OpenQA/WebSockets/Controller/Worker.pm
+++ lib/OpenQA/WebSockets/Controller/Worker.pm
@@ -147,20 +147,19 @@ sub _message {
         my $current_worker_error  = $current_worker_status eq 'broken' ? $json->{reason} : undef;
         my $job_info              = $json->{job} // {};
         my $job_status            = $job_info->{state};
-        my $job_id                = $job_info->{id};
+        my $job_id                = $job_info->{id} // '';
         my $job_settings          = $job_info->{settings} // {};
         my $job_token             = $job_settings->{JOBTOKEN};
         my $pending_job_ids       = $json->{pending_job_ids} // {};
-        my $wid                   = $worker->{id};
 
-        $worker_status->{$wid} = $json;
-        log_debug(sprintf('Received from worker "%u" worker_status message "%s"', $wid, dumper($json)));
+        $worker_status->{$worker_id} = $json;
+        log_debug(sprintf('Received from worker "%u" worker_status message "%s"', $worker_id, dumper($json)));
 
         # log that we 'saw' the worker
         try {
             $schema->txn_do(
                 sub {
-                    return undef unless my $w = $schema->resultset("Workers")->find($wid);
+                    return undef unless my $w = $schema->resultset("Workers")->find($worker_id);
                     log_debug("Updating worker seen from worker_status");
                     $w->seen;
                     $w->update({error => $current_worker_error});
@@ -179,10 +178,83 @@ sub _message {
         catch {
             log_debug("Could not send the population number to worker: $_");
         };
+
+        # send the currently assigned job again if the worker claims it does not work on any job
+
+        $self->_reconsider_jobs_assigned_to_worker($worker_id, $job_id, $pending_job_ids);
     }
     else {
         log_error(sprintf('Received unknown message type "%s" from worker %u', $message_type, $worker->{id}));
     }
+}
+
+sub _reconsider_jobs_assigned_to_worker {
+    my ($self, $worker_id, $job_id_worker_claims_to_work_on, $job_ids_worker_claims_to_be_pending) = @_;
+
+    my $schema         = $self->app->schema;
+    my $workers        = $schema->resultset('Workers');
+    my $worker_is_busy = $job_id_worker_claims_to_work_on || scalar keys %$job_ids_worker_claims_to_be_pending;
+    my @jobs_to_send;
+
+    try {
+        # find the jobs which are currently assigned to the worker; skip if there are none
+        my $worker = $workers->find($worker_id);
+        return undef unless defined $worker;
+        my @unfinished_jobs = $worker->unfinished_jobs;
+        my $current_job     = $worker->job // $unfinished_jobs[0];
+        return undef unless defined $current_job;
+
+        # skip any further actions if the worker is simply working on the one job it is supposed to work on
+        return undef if $current_job->id eq $job_id_worker_claims_to_work_on && scalar @unfinished_jobs <= 1;
+
+        # check whether the worker works an all jobs it is supposed to work on
+        my %considered_jobs;
+        for my $assigned_job ($current_job, @unfinished_jobs) {
+            next unless defined $assigned_job;
+
+            # prevent doing this twice for the same job ($current_job and @unfinished_jobs might overlap)
+            my $job_id = $associated_job->id;
+            next if exists $considered_jobs{$job_id};
+            $considered_jobs{$job_id} = 1;
+
+            # do nothing if the worker claims that it is still working on that job or that the job is still
+            # pending
+            next if $job_id eq $job_id_worker_claims_to_work_on;
+            next if exists $job_ids_worker_claims_to_be_pending->{$job_id};
+
+            # skip jobs which are not in assigned state
+            # note: Jobs in other states are marked as incomplete by the stall job detection. Marking jobs
+            #       as incomplete here as well is avoided because that operation can be quite costly.
+            #       We could however just cancel the web socket connection to force the worker to re-register.
+            next unless $associated_job->state eq ASSIGNED;
+
+            # mark job to be sent to the worker again if the worker claims it is not working on a job
+            # note: Sending the job assignment simply might have not worked initially due to network issues. If
+            #       the worker was actually busy it would have explicitely rejected the job and the assignment
+            #       would have been revoked. If the worker sent the idle status before processing the job assignment
+            #       this 2nd assignment will be ignored by the worker.
+            if (!$worker_is_busy) {
+                push(@jobs_to_send, $assigned_job);
+                next;
+            }
+
+            # re-schedule job if the worker works on a different job
+            try {
+                $schema->txn_do(sub { $associated_job->reschedule_state; });
+            }
+            catch {
+                log_warning("Unable to reschedule jobs abandoned by worker $worker_id: $_");
+            };
+        }
+
+        # send jobs to the worker again
+        return undef unless @jobs_to_send;
+        my %cluster_info_by_job_id;
+
+    }
+    catch {
+        log_error("Failed to re-send assigned jobs to worker $worker_id: $_");
+    };
 }
 
 1;
