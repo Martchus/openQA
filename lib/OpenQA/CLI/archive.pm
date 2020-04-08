5e6f7f698132a321c89b3633fa9b06016774c448
Sebastian Riedel <sri@cpan.org>
2020-04-08 18:11:49 +0200

    Implement openqa-cli archive subcommand

--- lib/OpenQA/CLI/archive.pm
+++ lib/OpenQA/CLI/archive.pm
@@ -16,13 +16,30 @@
 package OpenQA::CLI::archive;
 use Mojo::Base 'OpenQA::Command';
 
+use Mojo::Util qw(getopt);
+
 has description => 'Download assets and test results from a job';
 has usage       => sub { shift->extract_usage };
 
 sub run {
     my ($self, @args) = @_;
 
-    die "Not yet implemented!\n";
+    getopt \@args,
+      'apibase=s'         => \(my $base = '/api/v1'),
+      'apikey=s'          => \my $key,
+      'apisecret=s'       => \my $secret,
+      'H|host=s'          => \(my $host = 'http://localhost'),
+      't|with-thumbnails' => \my $thumbnails;
+
+    @args = $self->decode_args(@args);
+    die $self->usage unless my $job  = shift @args;
+    die $self->usage unless my $path = shift @args;
+
+    my $url = Mojo::URL->new($host);
+    $url->path($self->prepend_apibase($base, "jobs/$job/details"));
+
+    my $client = $self->client(apikey => $key, apisecret => $secret, api => $url->host);
+    $client->archive->run({url => $url, archive => $path, 'with-thumbnails' => $thumbnails});
 }
 
 1;
@@ -31,13 +48,16 @@ sub run {
 
 =head1 SYNOPSIS
 
-  Usage: openqa-cli archive [OPTIONS]
+  Usage: openqa-cli archive [OPTIONS] JOB PATH
 
     openqa-cli archive ...
 
   Options:
+        --apibase <path>        API base, defaults to /api/v1
         --apikey <key>          API key
         --apisecret <secret>    API secret
+    -H, --host <host>           Target host, defaults to http://localhost
     -h, --help                  Show this summary of available options
+    -t, --with-thumbnails       Download thumbnails as well
 
 =cut
